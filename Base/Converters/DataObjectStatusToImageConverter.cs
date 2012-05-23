f8a0054eb5aa0f68a6e27e4a763570881cffbe0d
Michael Seeger <miseeger@gmx.de>
2012-05-23 23:18:02 +0200

    Added resources and resource manager Added DataObjectStatusToImageConverter

---
+++ Base/Converters/DataObjectStatusToImageConverter.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Drawing;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+using MashedVVM.Base.Enum;
+using MashedVVM.Resources;
+
+namespace MashedVVM.Base.Converters
+{
+
+	[ValueConversion(typeof (DataObjectStatus), typeof (BitmapSource))]
+	public class DataObjectStatusToImageConverter : IValueConverter
+	{
+
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+
+			DataObjectStatus dataObjectStatus = (DataObjectStatus) value;
+			Bitmap bmpToConvert = MashedVVMResources.original;
+
+			BitmapSource result = null;
+
+			switch (dataObjectStatus)
+			{
+				case DataObjectStatus.Modified:
+					bmpToConvert = MashedVVMResources.modified;
+					break;
+				case DataObjectStatus.Deleted:
+					bmpToConvert = MashedVVMResources.deleted;
+					break;
+				case DataObjectStatus.Added:
+					bmpToConvert = MashedVVMResources.added;
+					break;
+			}
+
+			IntPtr hBitmap = bmpToConvert.GetHbitmap();
+			BitmapSizeOptions sizeOptions = BitmapSizeOptions.FromEmptyOptions();
+			result = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(hBitmap, IntPtr.Zero, Int32Rect.Empty, sizeOptions);
+			result.Freeze();
+
+			return result; 
+		}
+
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return DataObjectStatus.Original; ;
+		}
+	}
+
+}
---
+++ Resources/MashedVVMResources.cs
@@ -0,0 +1,102 @@
+﻿/* ************************************************************************* *
+ * MashedVVM                                                                 *
+ *                                                                           *
+ * Created with SharpDevelop (http://www.icsharpcode.net/OpenSource/SD/)     *
+ * By  : Michael Seeger (www.codedriven.net)                                 *
+ *                                                                           *
+ * Licensed under the MS-PL (http://www.opensource.org/licenses/MS-PL)       *
+ * ************************************************************************* */
+
+using System;
+
+namespace MashedVVM.Resources
+{
+
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+	[global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+	[global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+	
+	internal class MashedVVMResources
+	{
+
+		private static global::System.Resources.ResourceManager resourceMan;
+		private static global::System.Globalization.CultureInfo resourceCulture;
+
+		
+		[global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+		internal MashedVVMResources()
+		{
+		}
+
+
+		[global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
+		internal static global::System.Resources.ResourceManager ResourceManager 
+		{
+			get 
+			{
+				if (object.ReferenceEquals(resourceMan, null)) 
+				{
+					global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MashedVVM.Resources.MashedMVVMResources", typeof(MashedVVMResources).Assembly);
+					resourceMan = temp;
+				}
+				return resourceMan;
+			}
+		}
+
+
+		[global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
+		internal static global::System.Globalization.CultureInfo Culture {
+		get {
+				return resourceCulture;
+			}
+			set {
+				resourceCulture = value;
+			}
+		}
+
+
+		internal static System.Drawing.Bitmap added
+		{
+			get 
+			{
+				object obj = ResourceManager.GetObject("added", resourceCulture);
+				return ((System.Drawing.Bitmap)(obj));
+			}
+		}
+
+
+		internal static System.Drawing.Bitmap deleted
+		{
+			get 
+			{
+				object obj = ResourceManager.GetObject("deleted", resourceCulture);
+				return ((System.Drawing.Bitmap)(obj));
+			}
+		}
+
+
+		internal static System.Drawing.Bitmap modified
+		{
+			get 
+			{
+				object obj = ResourceManager.GetObject("modified", resourceCulture);
+				return ((System.Drawing.Bitmap)(obj));
+			}
+		}
+
+		
+		internal static System.Drawing.Bitmap original 
+		{
+			get 
+			{
+				object obj = ResourceManager.GetObject("original", resourceCulture);
+				return ((System.Drawing.Bitmap)(obj));
+			}
+		}
+
+
+	}
+}
+
+
+
