32361232020df0f67b88e87a72db98af55119172
Michael Seeger <miseeger@gmx.de>
2012-06-13 05:50:55 +0200

    Added codesnippets for SharpDevelop and testable class RelayCommandsToTest.

---
+++ Test/Framework/TestObjects/RelayCommandsToTest.cs
@@ -0,0 +1,29 @@
+﻿/* ************************************************************************* *
+ * MashedVVM.Test                                                            *
+ *                                                                           *
+ * Created with SharpDevelop (http://www.icsharpcode.net/OpenSource/SD/)     *
+ * By Michael Seeger (www.codedriven.net)                                    *
+ *                                                                           *
+ * This code is distributed under the MS Public License. For more details    *
+ * see http://www.opensource.org/licenses/MS-PL.                             *
+ *                                                                           *
+ * ************************************************************************* */
+
+using System;
+using System.Windows.Input;
+using MashedVVM.Framework;
+
+namespace MashedVVM.Test.Framework.TestObjects
+{
+
+	public class RelayCommandsToTest
+	{
+
+		public RelayCommandsToTest()
+		{
+		}
+
+	
+	}
+	
+}
