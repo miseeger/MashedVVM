f963a665532f928cb9a29ce2e7df1c6f44ad7180
Michael Seeger <michael_seeger@gmx.de>
2013-01-30 21:28:26 +0100

    Implemented IgnoreObjectStatus functionality so the use or only IsDirty is possible. It is then decoupled from ObjectStatus Property and ObjectStatus will not be used.

--- Base/DataObject.cs
+++ Base/DataObject.cs
@@ -10,8 +10,9 @@
  * ************************************************************************* */
 
 using System.Linq; 
-using MashedVVM.Base.Contracts; 
-using MashedVVM.Base.Enum; 
+using MashedVVM.Base.Attributes;
+using MashedVVM.Base.Contracts;
+using MashedVVM.Base.Enum;
 using MashedVVM.Resources; 
  
 namespace MashedVVM.Base 
@@ -24,64 +25,98 @@ namespace MashedVVM.Base
 		{ 
 			PropertyChanged += (o, e) => 
 			{ 
-				if (ObjectStatus == DataObjectStatus.Original 
-				    && (!Names.ObjectStatusIgnoringProperties.Contains(e.PropertyName)))
-				{ 
-					ObjectStatus = DataObjectStatus.Modified; 
-				} 
-			}; 
-		} 
- 
- 
+				if (IgnoreObjectStatus && e.PropertyName != "IsDirty") 
+				{
+					IsDirty = true;	
+				}
+				else 
+				{
+					if (ObjectStatus == DataObjectStatus.Original
+				    	&& (!Names.ObjectStatusIgnoringProperties.Contains(e.PropertyName)))
+					{ 
+						ObjectStatus = DataObjectStatus.Modified; 
+					} 
+				}
+			};
+			
+			IgnoreObjectStatusAttribute[] classAttributes = (IgnoreObjectStatusAttribute[])this.GetType()
+				.GetCustomAttributes(typeof(IgnoreObjectStatusAttribute), false);
+			IgnoreObjectStatus = (classAttributes.Any() && classAttributes[0].IgnoreObjectStatus);
+			
+			ObjectStatus = IgnoreObjectStatus ? DataObjectStatus.Ignore : DataObjectStatus.Original;
+		}
+		
+		
+		public bool IgnoreObjectStatus { get; private set; }
+
+		
 		private DataObjectStatus _objectStatus = DataObjectStatus.Original;
 		public DataObjectStatus ObjectStatus 
 		{ 
 			get { return _objectStatus; } 
 			set 
 			{ 
-				if (
-						(_objectStatus != value)
-				        && (
-							   (_objectStatus != DataObjectStatus.Added)
-							   && (
-									  (_objectStatus == DataObjectStatus.Original)
-							   		  || (_objectStatus == DataObjectStatus.Deleted && value == DataObjectStatus.Original)
-				            		  || (_objectStatus == DataObjectStatus.Modified && value != DataObjectStatus.Added)
-				            	   )
-							)
-				   )
-				{ 
-					_objectStatus = value;
-					IsDirty = (_objectStatus != DataObjectStatus.Original);
-					RaisePropertyChanged(() => ObjectStatus);
+				if (IgnoreObjectStatus) 
+				{
+					_objectStatus = DataObjectStatus.Ignore;
+				}
+				else
+				{
+					if (
+							(_objectStatus != value)
+				        	&& (
+								   (_objectStatus != DataObjectStatus.Added)
+								   && (
+									  	(_objectStatus == DataObjectStatus.Original)
+							   		  	|| (_objectStatus == DataObjectStatus.Deleted && value == DataObjectStatus.Original)
+				            		  	|| (_objectStatus == DataObjectStatus.Modified && value != DataObjectStatus.Added)
+				            	   	)
+								)
+				   	)
+				 	{ 
+						_objectStatus = value;
+						IsDirty = (_objectStatus != DataObjectStatus.Original);
+						RaisePropertyChanged(() => ObjectStatus);
+					}
 				}
 			} 
-		} 
- 
- 
+		}
+
+
 		private bool _isDirty = false; 
 		public bool IsDirty 
 		{ 
 			get { return _isDirty; } 
 			set 
 			{
-				if ((ObjectStatus != DataObjectStatus.Added && _isDirty != value) 
-				    || (ObjectStatus == DataObjectStatus.Added && _isDirty != value && value))
-				{	
-					_isDirty = value;
-					RaisePropertyChanged(() => IsDirty);
-					
-					if (_isDirty && ObjectStatus == DataObjectStatus.Original)
+				if (IgnoreObjectStatus) 
+				{
+					if (_isDirty != value)
 					{
-						ObjectStatus = DataObjectStatus.Modified;
+						_isDirty = value;
+						RaisePropertyChanged(() => IsDirty);
 					}
-					else
-					{
-						if (!_isDirty 
-						    && (ObjectStatus == DataObjectStatus.Modified
-						        || ObjectStatus == DataObjectStatus.Deleted))
+				}
+				else
+				{
+					if ((ObjectStatus != DataObjectStatus.Added && _isDirty != value)
+				    	|| (ObjectStatus == DataObjectStatus.Added && _isDirty != value && value))
+					{	
+						_isDirty = value;
+						RaisePropertyChanged(() => IsDirty);
+						
+						if (_isDirty && ObjectStatus == DataObjectStatus.Original)
 						{
-							ObjectStatus = DataObjectStatus.Original;
+							ObjectStatus = DataObjectStatus.Modified;
+						}
+						else
+						{
+							if (!_isDirty 
+							    && (ObjectStatus == DataObjectStatus.Modified
+							        || ObjectStatus == DataObjectStatus.Deleted))
+							{
+								ObjectStatus = DataObjectStatus.Original;
+							}
 						}
 					}
 				}
