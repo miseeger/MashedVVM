5b95f951af3fa74a8ac973579941d0de67308acf
Michael Seeger <michael_seeger@gmx.de>
2013-01-30 21:25:14 +0100

    Added IgnoreObjectStatusAttribute to be able to switch off ObjectStatus tracking in order to work with IsDirty only.

---
+++ Base/Attributes/IgnoreObjectStatusAttribute.cs
@@ -0,0 +1,29 @@
+﻿/* ************************************************************************* *
+ * MashedVVM                                                                 *
+ *                                                                           *
+ * Created with SharpDevelop (http://www.icsharpcode.net/OpenSource/SD/)     *
+ * By  : Michael Seeger (www.codedriven.net)                                 *
+ *                                                                           *
+ * This code is distributed under the MS Public License. For more details    *
+ * see http://www.opensource.org/licenses/MS-PL.                             *
+ *                                                                           *
+ * ************************************************************************* */
+
+using System;
+
+namespace MashedVVM.Base.Attributes
+{
+
+	[AttributeUsage(System.AttributeTargets.Class, Inherited = true, AllowMultiple = false)]
+	public sealed class IgnoreObjectStatusAttribute : System.Attribute
+	{
+		public bool IgnoreObjectStatus { get; private set; }
+
+		public IgnoreObjectStatusAttribute(bool ignoreObjectStatus)
+		{
+			IgnoreObjectStatus = ignoreObjectStatus;
+		}
+
+	}
+
+}
